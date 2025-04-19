fb0e91a2554fd826fb76e68af2247e0b161f3254
SWARNABHA SAHA <itsswarnabha@gmail.com>
2025-04-19 05:59:08 +0000

    Remove Maintenance, Care and Planning tabs

--- src/components/sidebar-items.tsx
+++ src/components/sidebar-items.tsx
@@ -16,32 +16,5 @@ export const SidebarItems = [
         href: "/tasks",
         title: "Tasks",
         icon: <Icons.listChecks/>
-    },
-    {
-        href: "/maintenance",
-        title: "Maintenance",
-        icon: <Icons.settings/>
-    },
-    {
-        href: "/care",
-        title: "Care",
-        icon: <Icons.plant/>,
-        subMenuItems: [
-            {
-                href: "/care/pets",
-                title: "Pets",
-                icon: <Icons.dog/>
-            },
-            {
-                href: "/care/plants",
-                title: "Plants",
-                icon: <Icons.plant/>
-            }
-        ]
-    },
-    {
-        href: "/planning",
-        title: "Planning",
-        icon: <Icons.calendar/>
     }
 ];
